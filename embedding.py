eff4f19f4fc71f2a7745e9b21b15427e840793fa
JuneJe0n <jyunejeon@gmail.com>
2025-02-15 15:28:57 +0900

    load dotenv

--- embedding.py
+++ embedding.py
@@ -1,6 +1,8 @@
 import os
 from openai import OpenAI
 from util import preprocess_prompt
+from dotenv import load_dotenv
+load_dotenv()
 
 # openai embedding api
 CLIENT = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
